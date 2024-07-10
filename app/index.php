33421159bafa6fad7bc03a467ae6d5921a516be7
Humberto Arouca <harouca@gmail.com>
2024-07-10 13:44:20 -0400

    Index Page

---
+++ app/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projeto LAMP em Docker</title>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h1>Olá, Mundo!</h1>
+            <p id="mensagem">Página inicial do Projeto LAMP em docker.</p>
+            <br>
+            <button id="botao">Clique aqui</button>
+        </div>
+    </div>
+</body>
+<script src="js/index.js" type="text/javascript"></script>
+</html>
